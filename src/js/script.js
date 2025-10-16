f6aa2af6c315212ebafd621ab6e7fbfe070a30d2
Andrii <kostivvandriyy79@gmail.com>
2025-10-16 11:03:00 +0200

    Foundation

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "/src/sass/style.scss";
+
+try {
+  new Swiper('.works__slider', {
+    slidesPerView: 1,
+    loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".icon-left-open",
+        prevEl: ".icon-right-open",
+      },
+      breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 5
+        },
+        1920: {
+          spaceBetween: 5,
+          spaceBetween: 35
+        }
+      },
+    modules: [Navigation, Pagination]
+  });
+} catch (e) {}
